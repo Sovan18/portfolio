77748f1544a6c09bc33f792ce8797899b073aac9
Sovan Jana <sovanjana122@gmail.com>
2025-12-14 19:49:14 +0530

    first

---
+++ my-portfolio/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import CustomCursor from "./components/CustomCursor";
+import Navbar from "./components/Navbar";
+import ParticlesBackground from "./components/ParticlesBackground";
+import About from "./sections/About";
+import Contact from "./sections/Contact";
+import Experience from "./sections/Experience";
+import Footer from "./sections/Footer";
+import Home from "./sections/Home";
+import Projects from "./sections/Projects";
+import Skills from "./sections/Skills";
+import Testimonials from "./sections/Testimonials";
+import IntroAnimation from "./components/IntroAnimation";
+
+export default function App(){
+  const [introDone,setIntroDone]=useState(false)
+  return(
+    <>
+    {!introDone && <IntroAnimation onFinish={()=>setIntroDone(true)}/>}
+    {introDone &&(
+    <div className="relative gradient text-white">
+      <CustomCursor/>
+     {/* <ParticlesBackground/>*/}
+      <Navbar/>
+      <Home/>
+      <About/>
+      <Skills/>
+      <Projects/>
+      <Experience/>
+      <Testimonials/>
+      <Contact/>
+      <Footer/>
+    </div>
+    )}
+    </>
+  )
+}
